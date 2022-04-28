f21814558b31519f22603c68b67b9296e7c79328
Dudewanted <hariz@yandex.ru>
2022-04-28 20:42:20 +0300

    Fix files in project step#9

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,20 +4,18 @@ from random import randint
 
 TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
+def game_data():
+    number = randint(2, 100)
+    question = 'Question: {}'.format(number)
+    if is_prime(number):
+        correct_answer = 'no'
+    else:
+        correct_answer = 'yes'
+    return question, correct_answer
 
 def is_prime(number):
     count = 0
     for i in range(1, number + 1):
         if number % i == 0:
             count = count + 1
-    return count <= 1
-
-
-def game_data():
-    number = randint(0, 100)
-    question = 'Question: {}'.format(number)
-    if is_prime(number):
-        correct_answer = 'yes'
-    else:
-        correct_answer = 'no'
-    return question, correct_answer
+    return count > 2
